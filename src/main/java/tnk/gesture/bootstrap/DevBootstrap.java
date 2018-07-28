5776d81eccb6c8aba0da8e4b673e70783edfb72c
Thomas <tom.n.knol@gmail.com>
2018-07-28 12:13:30 -0500

    updated to work with mongo and to create a user

--- src/main/java/tnk/gesture/bootstrap/DevBootstrap.java
+++ src/main/java/tnk/gesture/bootstrap/DevBootstrap.java
@@ -7,16 +7,25 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import tnk.gesture.model.Image;
 import tnk.gesture.model.Tag;
+import tnk.gesture.model.User;
 import tnk.gesture.repositories.ImageRepository;
 import tnk.gesture.repositories.TagRepository;
+import tnk.gesture.repositories.UserRepository;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
+    @Autowired private UserRepository userRepository;
+        @Autowired private TagRepository tagRepository;
     @Autowired private ImageRepository imageRepository;
-    @Autowired private TagRepository tagRepository;
 
     public void initData(){
+
+        User user = new User();
+        user.setUserName("tnk");
+        user.setFirstName("Tom");
+        user.setLastName("Knol");
+
         Image one = new Image("one", "one");
         Image two = new Image("two", "one");
         Tag figure = new Tag("figure", "figure");
@@ -24,15 +33,32 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
         one.getTags().add(figure);
         one.getTags().add(nude);
         two.getTags().add(figure);
-        figure.getImages().add(one);
-        figure.getImages().add(two);
-        nude.getImages().add(one);
 
+        user.getImages().add(one);
+        userRepository.save(user);
+        user.getImages().add(two);
+        user.getTags().add(figure);
+        user.getTags().add(nude);
+        userRepository.save(user);
+
+
+
+
+        userRepository.save(user);
         imageRepository.save(one);
         imageRepository.save(two);
-        tagRepository.save(nude);
         tagRepository.save(figure);
+        tagRepository.save(nude);
+//        figure.getImages().add(one);
+//        figure.getImages().add(two);
+//        nude.getImages().add(one);
+
+
+//        imageRepository.save(two);
+//        tagRepository.save(nude);
+//        tagRepository.save(figure);
     }
+
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
