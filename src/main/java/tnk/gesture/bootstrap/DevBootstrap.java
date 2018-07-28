5331f96907e6d7b6aeb58def91993656e08f9407
Thomas <tom.n.knol@gmail.com>
2018-07-28 17:54:33 -0500

    clean up dev data

--- src/main/java/tnk/gesture/bootstrap/DevBootstrap.java
+++ src/main/java/tnk/gesture/bootstrap/DevBootstrap.java
@@ -28,27 +28,39 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         Image one = new Image("one", "one");
         Image two = new Image("two", "one");
-        Tag figure = new Tag("figure", "figure");
-        Tag nude = new Tag("nude", "nude");
+        Tag figure = new Tag();
+        figure.setName("figure");
+        Tag nude = new Tag();
+        nude.setName("nude");
+        user.getTags().add(figure);
+        user.getTags().add(nude);
         one.getTags().add(figure);
         one.getTags().add(nude);
         two.getTags().add(figure);
 
         user.getImages().add(one);
-        userRepository.save(user);
         user.getImages().add(two);
-        user.getTags().add(figure);
-        user.getTags().add(nude);
-        userRepository.save(user);
-
-
-
 
         userRepository.save(user);
         imageRepository.save(one);
         imageRepository.save(two);
         tagRepository.save(figure);
         tagRepository.save(nude);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //        figure.getImages().add(one);
 //        figure.getImages().add(two);
 //        nude.getImages().add(one);
