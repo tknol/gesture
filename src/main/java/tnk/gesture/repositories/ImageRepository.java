58fa07bb6b147ad62c60b23f69c6317f9a4a9e4d
Thomas <tom.n.knol@gmail.com>
2018-07-29 11:35:38 -0500

    adding a customer image repo

--- src/main/java/tnk/gesture/repositories/ImageRepository.java
+++ src/main/java/tnk/gesture/repositories/ImageRepository.java
@@ -3,7 +3,11 @@ package tnk.gesture.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 import tnk.gesture.model.Image;
+import tnk.gesture.model.Tag;
+
+import java.util.HashSet;
 
 @Component
-public interface ImageRepository extends CrudRepository<Image, String> {
+public interface ImageRepository extends CrudRepository<Image, String>, ImageRepositoryCustom {
+
 }
