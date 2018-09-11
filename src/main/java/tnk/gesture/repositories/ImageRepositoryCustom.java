9721cef6d474d3d77e1f7507ed0f40b9013c2dbf
Thomas <tom.n.knol@gmail.com>
2018-09-11 13:58:59 -0500

    cleanup unused imports

--- src/main/java/tnk/gesture/repositories/ImageRepositoryCustom.java
+++ src/main/java/tnk/gesture/repositories/ImageRepositoryCustom.java
@@ -1,12 +1,8 @@
 package tnk.gesture.repositories;
 
-import org.springframework.data.jpa.repository.Query;
 import tnk.gesture.model.Image;
 import tnk.gesture.model.Tag;
-
 import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
 
 public interface ImageRepositoryCustom {
 
