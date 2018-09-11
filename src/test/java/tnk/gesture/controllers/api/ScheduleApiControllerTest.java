d25b3c17bd920917a5ce0396f156aa4cb0168495
Thomas <tom.n.knol@gmail.com>
2018-09-11 14:40:22 -0500

    adding an unimplemented test

---
+++ src/test/java/tnk/gesture/controllers/api/ScheduleApiControllerTest.java
@@ -0,0 +1,12 @@
+package tnk.gesture.controllers.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScheduleApiControllerTest {
+
+    @Test
+    public void getSchedules() {
+    }
+}
