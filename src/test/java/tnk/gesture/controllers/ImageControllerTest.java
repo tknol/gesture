663864df7e3a2fa955802cc458a2653087351bc2
Thomas <tom.n.knol@gmail.com>
2018-09-11 14:11:52 -0500

    cleanup unused imports

--- src/test/java/tnk/gesture/controllers/ImageControllerTest.java
+++ src/test/java/tnk/gesture/controllers/ImageControllerTest.java
@@ -4,34 +4,21 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import tnk.gesture.commands.ImageCommand;
 import tnk.gesture.constants.Mappings;
-import tnk.gesture.model.Image;
 import tnk.gesture.model.User;
 import tnk.gesture.services.ImageService;
-import tnk.gesture.services.UserService;
-
-import java.util.ArrayList;
-import java.util.HashSet;
 
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class ImageControllerTest {
 
